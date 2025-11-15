04294eacb2425be9557e27e93215832b0403a016
fahrulriadu <fahrul@fahrul-linux.bbrouter>
2025-11-15 03:47:43 -0500

    fix: perbaikan error deactivated widget pada edit profile screen 4

--- lib/services/system_notifier.dart
+++ lib/services/system_notifier.dart
@@ -2,7 +2,6 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:timezone/timezone.dart' as tz;
 import 'package:timezone/data/latest.dart' as tz;
-import 'package:flutter/material.dart'; 
 
 class SystemNotifier {
   static final SystemNotifier _instance = SystemNotifier._internal();
